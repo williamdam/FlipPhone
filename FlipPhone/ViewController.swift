7a062f9cc538ca1de994fd9fc0ae5e4332f28cfd
William Dam <damw@oregonstate.edu>
2020-12-11 01:21:06 -0800

    UIImage View added.  Updated math.  Increase precision.  Proof of concept achieved.

--- FlipPhone/ViewController.swift
+++ FlipPhone/ViewController.swift
@@ -10,69 +10,183 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var startCountingButton: UIButton!
-    @IBOutlet weak var stopCountingButton: UIButton!
-    @IBOutlet weak var maxRotationSpeed: UILabel!
+    @IBOutlet weak var startCountingButton: UIButton!       // Button Start
+    @IBOutlet weak var stopCountingButton: UIButton!        // Button Stop
+    @IBOutlet weak var maxRotationSpeedLabel: UILabel!      // Label Max Rotation Speed
+    @IBOutlet weak var numRotationsLabel: UILabel!          // Label Num Rotations
+    
+    var motion = CMMotionManager()      // Init motion manager
+    var maxSpeed:Double = 0.0           // Max rotational speed
+    var maxG:Double = 0.0               // Max G's
+    var finishLine:Bool = true          // Flag for finish line
+    var checkPoint:Bool = false         // Flag for check point
+    var numRotations:Int = 0            // Number of rotations
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set UILabels to invisible
+        maxRotationSpeedLabel.alpha = 0.0
+        numRotationsLabel.alpha = 0.0
 
     }
-
-    var motion = CMMotionManager()
-
-    var maxSpeed = 0.0
-    var maxG = 0.0
     
-    // Gets rotational velocity in Rad/Sec
-    func MyGyro() {
-        motion.gyroUpdateInterval = 0.05    // Check for change every .5  sec
-        motion.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
-            if let trueData = data {
-                print(trueData)
-                print("Y: \(trueData.rotationRate.y)")
-                if abs(trueData.rotationRate.y) > self.maxSpeed {
-                    self.maxSpeed = abs(trueData.rotationRate.y)
-                }
-            }
+    
+    // Set rotation passed halfway point at 3 radians (PENDING: Increase precision with pi)
+    func setCheckPoint(_ rotationAngle:Double) {
+        
+        // Checkpoint located between 2.8 and 3.2 radians.
+        // If finishLine flagged, flag checkPoint, and unflag finishLine
+        if abs(rotationAngle) > 2.8 && abs(rotationAngle) < 3.2 && self.finishLine == true {
+            
+            self.checkPoint = true
+            self.finishLine = false
         }
+        
+
     }
     
-    // Gets force in G's per axis.  Device at rest should show ~1 G
-    func MyAccel() {
-        motion.accelerometerUpdateInterval = 0.05
-        motion.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
-            if let accelData = data {
-                print("Accel: \(accelData)")
+    // Set rotation passed finish line at 0 radians (PENDING: Increase precision with pi)
+    func setFinishLine(_ rotationAngle:Double) {
+        
+        // Finish line located at 0 radians, threshold +/- 0.8
+        // If checkPoint flagged, flag finishLine, and unflag checkPoint
+        if abs(rotationAngle) > 0 && abs(rotationAngle) < 0.8 && self.checkPoint == true {
+            
+            self.finishLine = true
+            self.checkPoint = false
+            
+            // Increment counter
+            self.numRotations += 1
+        }
+        
+    }
+    
+    // Function: MyGyro()
+    // Args: None
+    // Returns: Void
+    // Description: Gets gyroscope data from device and sets maxSpeed with rad/sec
+    func MyGyro() {
+        
+        // Check for change every 0.01  sec
+        motion.gyroUpdateInterval = 0.01
+        
+        // Start gyroscope
+        motion.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+            
+            if let trueData = data {
+                
+                // Print to console
+                print(trueData)
+                print("Rotation rate y: \(trueData.rotationRate.y)")
+                
+                // Update maxSpeed with rotationRate
+                if abs(trueData.rotationRate.y) > self.maxSpeed {
+                    
+                    self.maxSpeed = abs(trueData.rotationRate.y)    // (PENDING: Convert to rps or rpm)
+                }
                 
             }
         }
     }
     
+    // Function: MyAccel()
+    // Args: None
+    // Returns: Void
+    // Description: Gets accelerometer data from device and sets maxG with number of G's
+    func MyAccel() {
+        
+        // Check for change every 0.01 sec
+        motion.accelerometerUpdateInterval = 0.01
+        
+        // Start accelerometer
+        motion.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let accelData = data {
+                
+                // Print to console
+                print("Accel: \(accelData)")
+                
+                // Update maxG with number of G's
+                if accelData.acceleration.z > self.maxG {
+                    
+                    self.maxG = accelData.acceleration.z        // (PENDING: Check data for +/- G's)
+                }
+                
+            }
+            
+            // Automatically stop motion data on G sensor shock
+            /*if self.maxG > 1.55 {
+                self.motion.stopAccelerometerUpdates()
+                self.motion.stopGyroUpdates()
+                self.motion.stopDeviceMotionUpdates()
+                self.maxRotationSpeedLabel.text = String(format: "%.2f rps", self.maxSpeed)
+                self.numRotationsLabel.text = String(self.numRotations)
+            }*/
+        }
+    }
+    
+    // Function: MyAttitude()
+    // Args: None
+    // Returns: Void
+    // Description: Gets attitude of device around the Y-axis, then calls
+    // functions, setCheckPoint() and setFinishLine()
     func MyAttitude() {
-        motion.deviceMotionUpdateInterval = 0.05
+        
+        // Check for change every 0.01 sec
+        motion.deviceMotionUpdateInterval = 0.01
+        
+        // Start getting device attitude roll (Y-axis)
         motion.startDeviceMotionUpdates(to: OperationQueue.current!) { (data, error) in
             if let attitudeData = data {
+                
+                // Print to console
                 print("Attitude: \(attitudeData.attitude)")
+                
+                // Call helper functions
+                self.setCheckPoint(attitudeData.attitude.roll)
+                self.setFinishLine(attitudeData.attitude.roll)
+                
             }
+            
         }
     }
 
+    // Action: User presses start button
+    // Description: Set labels to invisible.  Call all motion functions.
     @IBAction func startCountingButtonPressed(_ sender: UIButton) {
         
-        //MyAttitude()
-        //MyGyro()
+        // Make labels invisible
+        maxRotationSpeedLabel.alpha = 0.0
+        numRotationsLabel.alpha = 0.0
+        
+        MyAttitude()
+        MyGyro()
         MyAccel()
         
     }
+    
+    // Action: User presses stop button
+    // Description: Stop all motion updates.
     @IBAction func stopCountingButtonPressed(_ sender: UIButton) {
         
+        // Stop motion updates
         motion.stopDeviceMotionUpdates()
         motion.stopGyroUpdates()
         motion.stopAccelerometerUpdates()
-        maxRotationSpeed.text = String(maxSpeed)
+        
+        // Set speed label to max rad/sec
+        maxRotationSpeedLabel.text = String(format: "%.2f rad/sec", self.maxSpeed)
+        
+        // Set num rotations label to total rotations
+        numRotationsLabel.text = String("\(numRotations) times")
+        
+        // Make both labels visible
+        maxRotationSpeedLabel.alpha = 1.0
+        numRotationsLabel.alpha = 1.0
+        
+        // Reset vars
         self.maxSpeed = 0.0
+        self.numRotations = 0
     }
     
     
